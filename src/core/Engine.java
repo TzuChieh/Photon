0fe124232e549763dc64dd49b86569f8db598f0c
DoFN <b01502051@ntu.edu.tw>
2016-06-22 22:20:15 +0800

    delete old branch

--- src/core/Engine.java
+++ src/core/Engine.java
@@ -84,18 +84,18 @@ public class Engine
 		
 		sampleManager = new SampleManager(FRAME_WIDTH_PX, FRAME_HEIGHT_PX);
 		
-		scene = new FiveBallsScene();
+//		scene = new FiveBallsScene();
 //		scene = new ClassicMaterialScene();
 //		scene = new LamborghiniScene();
 //		scene = new SponzaScene();
 		
 //		String filename = "./resource/radiosity/blocks.tri.7000";
-////		String filename = "./resource/radiosity/church.tri.9300";
-////		String filename = "./resource/radiosity/blocks.tri.fin";
-////		String filename = "./resource/radiosity/hall.tri.4500";
-////		String filename = "./resource/radiosity/room.tri.12100";
-//		RadiosityDatReader radReader = new RadiosityDatReader(filename);
-//		scene = radReader.parse();
+		String filename = "./resource/radiosity/church.tri.9300";
+//		String filename = "./resource/radiosity/blocks.tri.fin";
+//		String filename = "./resource/radiosity/hall.tri.4500";
+//		String filename = "./resource/radiosity/room.tri.12100";
+		RadiosityDatReader radReader = new RadiosityDatReader(filename);
+		scene = radReader.parse();
 		
 		
 		scene.cookScene();
